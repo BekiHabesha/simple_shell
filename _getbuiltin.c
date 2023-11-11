4d88963bbd7c1623e554431de5049c0cfb54d922
BekiHabesha <bereketdere@gmail.com>
2023-11-11 23:46:26 +0300

    _getbuiltin.c file - 0x16. C - Simple Shell _getbuiltin.c file Authored by Bereket Dereje

---
+++ _getbuiltin.c
@@ -0,0 +1,40 @@
+/*
+   * File_name: _getbuiltin.c file
+   * Created: 11th of November, 2023
+   * Author: Bereket Dereje Mekonnen
+   * GitHub repository: simple_shell
+   * Project: 0x16. C - Simple Shell
+   * Directory: simple_shell
+   */
+
+#include "shell.h"
+
+/**
+  * get_builtin - The builtin that pais the command in the arg
+  *
+  * @cmd: command
+  *
+  * Return: function pointer of the builtin command
+  */
+
+int (*get_builtin(char *cmd))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int i;
+
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[i].f);
+}
