f00546d328bd9d0489d4f647f5707d00eb43e9fe
BekiHabesha <bereketdere@gmail.com>
2023-11-12 00:05:20 +0300

    cd_shell.c file - 0x16. C - Simple Shell cd_shell.c file Authored by Bereket Dereje

---
+++ cd_shell.c
@@ -0,0 +1,58 @@
+/*
+   * File_name: cd_shell.c file
+   * Functions: cd_shell
+   * Created: 11th of November, 2023
+   * Author: Bereket Dereje Mekonnen
+   * GitHub repository: simple_shell
+   * Project: 0x16. C - Simple Shell
+   * Directory: simple_shell
+   */
+
+#include "shell.h"
+
+/**
+  * cd_shell - changes the current directory
+  *
+  * @datash: The data relevant
+  *
+  * Return: 1 on success
+  */
+
+int cd_shell(data_shell *datash)
+{
+	char *dir;
+	int ishome, ishome2, isddash;
+
+	dir = datash->args[1];
+
+	if (dir != NULL)
+	{
+		ishome = _strcmp("$HOME", dir);
+		ishome2 = _strcmp("~", dir);
+		isddash = _strcmp("--", dir);
+	}
+
+	if (dir == NULL || !ishome || !ishome2 || !isddash)
+	{
+		cd_to_home(datash);
+
+		return (1);
+	}
+
+	if (_strcmp("-", dir) == 0)
+	{
+		cd_previous(datash);
+		return (1);
+	}
+
+	if (_strcmp(".", dir) == 0 || _strcmp("..", dir) == 0)
+	{
+		cd_dot(datash);
+
+		return (1);
+	}
+
+	cd_to(datash);
+
+	return (1);
+}
